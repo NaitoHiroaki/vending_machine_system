69f9b48763465a61905d08c29f91443fa3061e70
hiroaki-n <naitoh0414@gmail.com>
2024-03-12 18:52:38 +0900

    7-1 5.マイグレーションの作成

---
+++ database/migrations/2024_03_12_082509_create_articles_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name', 20);
+            $table->string('email', 255);
+            $table->string('password', 20);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_id', 255);
+            $table->string('product_name', 20);
+            $table->string('price', 20);
+            $table->string('stock', 20);
+            $table->string('comment', 200)->nullable();
+            $table->longText('img_path')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id', 255);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name', 20);
+            $table->string('street_address', 200);
+            $table->string('representative_name', 20);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('sales');
+        Schema::dropIfExists('companies');
+    }
+};

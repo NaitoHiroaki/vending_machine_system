a0053bacf4846b2103ef6e42c4b9774d8a6201f4
naito-hiroaki <naitoh010@gmail.com>
2024-09-22 23:21:54 +0900

    ダミーデータ追加

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'cytech_step7'),
+            'database' => env('DB_DATABASE', 'vending_machine_system'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', ''),
